b6ea4097d2c05cfa6b33136b4cbd56dfe972c551
Jhonatan <jhonatan.medeiros02@gmail.com>
2017-11-08 00:51:25 -0200

    Alterado o database url do mongodb.

--- config/main.js
+++ config/main.js
@@ -2,7 +2,8 @@ module.exports = {
     // Secret key for JWT signing and encryption
     'secret': 'super secret passphrase',
     // Database connection information
-    'database': 'mongodb://localhost:27017/blog-database',
+    // 'database': 'mongodb://localhost:27017/blog-database',
+    'database': 'mongodb://jhonatan:minhasenha123@ds249605.mlab.com:49605/db_blog_an',
     // Setting port for server
     'port': process.env.PORT || 3000
 }
